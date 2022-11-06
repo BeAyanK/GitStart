2c75b175ebc2ddfd98cd411dd4ae13baba85c316
BeAyanK <beaayank@gmail.com>
2022-11-06 11:00:19 +0530

    pseudoCode -1

--- oops.js
+++ oops.js
@@ -255,69 +255,108 @@
 
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
-//Form Submit Event
+// Form submit event
 form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
-//DELETE EVENT
-itemList.addEventListener('click', removeItem)
-
-//Add Item
+// Add item
 function addItem(e){
-    e.preventDefault();
+  e.preventDefault();
 
-    //Get Input Values
-    var newItem = document.getElementById('item').value;
+  // Get input value
+  var newItem = document.getElementById('item').value;
+  var newDesc = document.getElementById('desc').value;
 
-    //Create new li element
-    var li = document.createElement('li');
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
 
-    //Add Class
-    li.className = 'list-group-item';
+  // Create del button element
+  var deleteBtn = document.createElement('button');
 
-    //Append TextNode with Input Values
-    li.appendChild(document.createTextNode(newItem));
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 
-    //Create Del Btn
-    var delBtn = document.createElement('button');
-    
-    //Add Class to DelBtn
-    delBtn.className = 'btn btn-danger btn-sm float-right delete';
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
 
-    //Append TextNode
-    delBtn.appendChild(document.createTextNode('X'));
+  // Append button to li
+  li.appendChild(deleteBtn);
 
-    //Append delBtn to li
-    li.appendChild(delBtn);
-
-    //Create and Add Edit btn
+  //Edit Btn
     var editBtn = document.createElement('button');
     editBtn.className = 'btn btn-secondary btn-sm float-right edit';
     editBtn.appendChild(document.createTextNode('Edit'));
 
-    li.insertAdjacentElement("afterbegin", editBtn);
+    li.appendChild(editBtn);
+    
+    li.appendChild(document.createElement('br'));
 
-    //Append li to list
-    itemList.appendChild(li);
+    li.appendChild(document.createTextNode(newDesc));
 
+  // Append li to list
+  itemList.appendChild(li);
 }
 
+// Remove item
 function removeItem(e){
-    //e.preventDefault();
-    if (e.target.classList.contains('delete')){
-        if (confirm('Are You Sure?')){
-            var li = e.target.parentElement;
-            itemList.removeChild(li);
-        }
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
     }
+  }
 }
 
-//Create Edit btn
+// Description
+var item = document.querySelector('#item');
+item.setAttribute('placeholder', 'Item');
+
+var desc = document.createElement('input');
+desc.setAttribute('type','text');
+desc.setAttribute('placeholder','Description');
+desc.className = 'form-control mr-2';
+desc.id = 'desc';
+
+var form = document.querySelector('form');
+var btn = document.querySelector('form .btn-dark')
+
+form.insertBefore(desc,btn);
+
+//Edit Button for each item
 var items = document.getElementsByTagName('li');
 
 for (var i=0;i<items.length;i++){
     var editBtn = document.createElement('button');
     editBtn.className = 'btn btn-secondary btn-sm float-right edit';
     editBtn.appendChild(document.createTextNode('Edit'));
-    items[i].insertAdjacentElement("afterbegin", editBtn);;
+    items[i].insertAdjacentElement("beforeend", editBtn);
+}
+
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    console.log(item);
+    var itemName = item.firstChild.textContent;
+    var itemDesc = item.lastChild.textContent;
+    console.log(itemName, itemDesc);
+    if((itemName.toLowerCase().indexOf(text) != -1) || (itemDesc.toLowerCase().indexOf(text) != -1)){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
 }
